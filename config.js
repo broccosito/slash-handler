56ff2dd022574959bf631a88b39887e0a8317bdb
Juan Martinez <hello@enjoyablee.me>
2022-03-10 22:05:48 -0500

    Updated Fallback

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const { Intents } = require("discord.js");
 
 const config = {
   bot: {
-    token: "" || process.env.DISCORD_TOKEN,
+    token: process.env.DISCORD_TOKEN || "",
     intents: [], // You can find the available intents on https://discord.js.org/#/docs/main/stable/class/Intents?scrollTo=s-FLAGS
     guildId: "", // Update this field to only register commands a guild, this will make the commands to load instantly in the selected gulld (Optional)
   },
